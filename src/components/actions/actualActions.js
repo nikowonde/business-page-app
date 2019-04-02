57811f78e7335a000efa45d046397474862eee6c
nikowonde <45310446+nikowonde@users.noreply.github.com>
2019-04-02 16:13:10 +0300

    change
    description

--- src/components/actions/actualActions.js
+++ src/components/actions/actualActions.js
@@ -4,33 +4,44 @@ import Axios from 'axios';
 
 export const getPosts = () => {
 	return (dispatch) => {
-		Axios.get('https://business-json.herokuapp.com/posts')
-			.then((res) => {
-				//console.log(res.data);
-				dispatch({
-					type: GET_POSTS,
-					payload: res.data
-				});
-			})
+		Axios.post('https://business-json.herokuapp.com/login', {
+			email: 'nikowonde@gmail.com',
+			password: 'salasana'
+		})
+			.then(
+				Axios.get('https://business-json.herokuapp.com/posts').then((res) => {
+					//console.log(res.data);
+					dispatch({
+						type: GET_POSTS,
+						payload: res.data
+					});
+				})
+			)
 			.catch((err) => console.log(err));
 	};
 };
 
 export const deletePost = (id) => {
 	return (dispatch) => {
-		Axios.get(`https://business-json.herokuapp.com/posts/${id}`)
-			.then((res) => {
-				console.log(res.data);
-				dispatch({
-					type: DELETE_POST,
-					payload: res.data,
-					id: id
-				});
-			})
+		Axios.post('https://business-json.herokuapp.com/login', {
+			email: 'nikowonde@gmail.com',
+			password: 'salasana'
+		})
 			.then(
-				Axios.delete(`https://business-json.herokuapp.com/posts/${id}`).then((res) => {
-					console.log(res);
-				})
+				Axios.get(`https://business-json.herokuapp.com/posts/${id}`)
+					.then((res) => {
+						console.log(res.data);
+						dispatch({
+							type: DELETE_POST,
+							payload: res.data,
+							id: id
+						});
+					})
+					.then(
+						Axios.delete(`https://business-json.herokuapp.com/posts/${id}`).then((res) => {
+							console.log(res);
+						})
+					)
 			)
 			.catch((err) => console.log(err));
 	};
@@ -38,37 +49,47 @@ export const deletePost = (id) => {
 
 export const undoPost = (undoData) => {
 	return () => {
-		Axios.post(`https://business-json.herokuapp.com/posts`, {
-			id: undoData.id,
-			sender: undoData.sender,
-			email: undoData.email,
-			subject: undoData.subject,
-			message: undoData.message
+		Axios.post('https://business-json.herokuapp.com/login', {
+			email: 'nikowonde@gmail.com',
+			password: 'salasana'
 		})
-			.then((res) => {
-				//console.log(res.data);
-			})
+			.then(
+				Axios.post(`https://business-json.herokuapp.com/posts`, {
+					id: undoData.id,
+					sender: undoData.sender,
+					email: undoData.email,
+					subject: undoData.subject,
+					message: undoData.message
+				}).then((res) => {
+					//console.log(res.data);
+				})
+			)
 			.catch((err) => console.log(err));
 	};
 };
 
 export const newPost = (tmp) => {
 	return (dispatch) => {
-		Axios.post(`https://business-json.herokuapp.com/posts`, {
-			id: tmp.id,
-			sender: tmp.sender,
-			email: tmp.email,
-			subject: tmp.subject,
-			message: tmp.message
+		Axios.post('https://business-json.herokuapp.com/login', {
+			email: 'nikowonde@gmail.com',
+			password: 'salasana'
 		})
-			.then(() => {
-				//console.log(res.data);
-				/*setTimeout(() => {
+			.then(
+				Axios.post(`https://business-json.herokuapp.com/posts`, {
+					id: tmp.id,
+					sender: tmp.sender,
+					email: tmp.email,
+					subject: tmp.subject,
+					message: tmp.message
+				}).then(() => {
+					//console.log(res.data);
+					/*setTimeout(() => {
                 }, 5000);*/
-				dispatch({
-					type: 'SET_UNDEFINED'
-				});
-			})
+					dispatch({
+						type: 'SET_UNDEFINED'
+					});
+				})
+			)
 			.catch((err) => console.log(err));
 	};
 };
